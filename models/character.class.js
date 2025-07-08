5b791d8782036220fdec7a15b41aafd0f7a3a71f
XDsicht <tobias.froehler24@gmail.com>
2025-07-08 18:14:47 +0200

    added first idle images to ch and en

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,8 @@
 class Character extends MovableObject {
 
+    constructor() {
+        super().loadImage('img/2.character/1.idle/Warrior_03__IDLE_000.png');
+    }
     jump() {
     }
 }
--- models/troll.class.js
+++ models/troll.class.js
@@ -1,2 +1,6 @@
 class Troll extends MovableObject {
+
+    constructor() {
+        super().loadImage('img/3.enemy/1.idle/Troll_03_1_IDLE_000.png');
+    }
 }
