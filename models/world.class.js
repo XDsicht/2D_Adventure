371ad44bdae1d812d89b693f6f34842d05197527
XDsicht <tobias.froehler24@gmail.com>
2025-07-09 19:23:25 +0200

    display of npcs

--- models/world.class.js
+++ models/world.class.js
@@ -6,10 +6,28 @@ class World {
         new Troll(),
         new Troll()
     ];
+    canvas;
+    ctx;
 
+    constructor(canvas) {
+        this.ctx = canvas.getContext("2d");
+        this.canvas = canvas;
+        this.draw();
+    }
 
 
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Canvas leeren
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.height, enemy.width);
+        });
+
+        // draw wird immer wieder aufgerufen, damit die Animationen laufen
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
     }
 }
