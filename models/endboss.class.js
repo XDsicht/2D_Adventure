9cb514543876dd8d9ef1c16b6f0a5c56603b2e26
XDsicht <tobias.froehler24@gmail.com>
2025-12-04 17:08:58 +0100

    added several pictures to codebase

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -16,7 +16,46 @@ class Endboss extends MovableObject {
         'img/4.boss/2.walk/Walk_007.png',
         'img/4.boss/2.walk/Walk_008.png',
         'img/4.boss/2.walk/Walk_009.png',
-    ]
+    ];
+
+    IMAGES_HURT = [
+        'img/4.boss/6.hurt/Hurt_000.png',
+        'img/4.boss/6.hurt/Hurt_001.png',
+        'img/4.boss/6.hurt/Hurt_002.png',
+        'img/4.boss/6.hurt/Hurt_003.png',
+        'img/4.boss/6.hurt/Hurt_004.png',
+        'img/4.boss/6.hurt/Hurt_005.png',
+        'img/4.boss/6.hurt/Hurt_006.png',
+        'img/4.boss/6.hurt/Hurt_007.png',
+        'img/4.boss/6.hurt/Hurt_008.png',
+        'img/4.boss/6.hurt/Hurt_009.png',
+    ];
+
+    IMAGES_DEAD = [
+        'img/4.boss/7.dead/Dead_000.png',
+        'img/4.boss/7.dead/Dead_001.png',
+        'img/4.boss/7.dead/Dead_002.png',
+        'img/4.boss/7.dead/Dead_003.png',
+        'img/4.boss/7.dead/Dead_004.png',
+        'img/4.boss/7.dead/Dead_005.png',
+        'img/4.boss/7.dead/Dead_006.png',
+        'img/4.boss/7.dead/Dead_007.png',
+        'img/4.boss/7.dead/Dead_008.png',
+        'img/4.boss/7.dead/Dead_009.png',
+    ];
+
+    IMAGES_ATTACKING = [
+        'img/4.boss/5.attack/Attack_000.png',
+        'img/4.boss/5.attack/Attack_001.png',
+        'img/4.boss/5.attack/Attack_002.png',
+        'img/4.boss/5.attack/Attack_003.png',
+        'img/4.boss/5.attack/Attack_004.png',
+        'img/4.boss/5.attack/Attack_005.png',
+        'img/4.boss/5.attack/Attack_006.png',
+        'img/4.boss/5.attack/Attack_007.png',
+        'img/4.boss/5.attack/Attack_008.png',
+        'img/4.boss/5.attack/Attack_009.png',
+    ];
 
     constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
--- models/troll.class.js
+++ models/troll.class.js
@@ -1,44 +1,83 @@
 class Troll extends MovableObject {
-    height = 240;
-    width = 240;
-    y = 225;
-    otherDirection = true;
+  height = 240;
+  width = 240;
+  y = 225;
+  otherDirection = true;
 
-    offset = {
-        top: 85,
-        left: 90,
-        right: 60,
-        bottom: 35,
-    };
+  offset = {
+    top: 85,
+    left: 90,
+    right: 60,
+    bottom: 35,
+  };
 
-    IMAGES_WALKING = [
-        'img/3.enemy/2.walk/Troll_03_1_WALK_000.png',
-        'img/3.enemy/2.walk/Troll_03_1_WALK_001.png',
-        'img/3.enemy/2.walk/Troll_03_1_WALK_002.png',
-        'img/3.enemy/2.walk/Troll_03_1_WALK_003.png',
-        'img/3.enemy/2.walk/Troll_03_1_WALK_004.png',
-        'img/3.enemy/2.walk/Troll_03_1_WALK_005.png',
-        'img/3.enemy/2.walk/Troll_03_1_WALK_006.png',
-        'img/3.enemy/2.walk/Troll_03_1_WALK_007.png',
-        'img/3.enemy/2.walk/Troll_03_1_WALK_008.png',
-        'img/3.enemy/2.walk/Troll_03_1_WALK_009.png',
-    ];
+  IMAGES_WALKING = [
+    "img/3.enemy/2.walk/Troll_03_1_WALK_000.png",
+    "img/3.enemy/2.walk/Troll_03_1_WALK_001.png",
+    "img/3.enemy/2.walk/Troll_03_1_WALK_002.png",
+    "img/3.enemy/2.walk/Troll_03_1_WALK_003.png",
+    "img/3.enemy/2.walk/Troll_03_1_WALK_004.png",
+    "img/3.enemy/2.walk/Troll_03_1_WALK_005.png",
+    "img/3.enemy/2.walk/Troll_03_1_WALK_006.png",
+    "img/3.enemy/2.walk/Troll_03_1_WALK_007.png",
+    "img/3.enemy/2.walk/Troll_03_1_WALK_008.png",
+    "img/3.enemy/2.walk/Troll_03_1_WALK_009.png",
+  ];
 
-    constructor() {
-        super().loadImage('img/3.enemy/1.idle/Troll_03_1_IDLE_000.png');
-        this.loadImages(this.IMAGES_WALKING);
-        this.moveLeft();
-        this.x = 600 + Math.random() * 500;
-        this.speed = 0.15 + Math.random() * 0.25;
-        this.animate();
-    }
+  IMAGES_HURT = [
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_000.png",
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_001.png",
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_002.png",
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_003.png",
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_004.png",
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_005.png",
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_006.png",
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_007.png",
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_008.png",
+    "img/3.enemy/6.hurt/Troll_03_1_HURT_009.png",
+  ];
 
-    animate() {
-        setInterval(() => {
-            this.moveLeft();
-        }, 1000 / 60);
-        setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
-        }, 100);
-    }
+  IMAGES_DEAD = [
+    'img/3.enemy/7.dead/Troll_03_1_DIE_000.png',
+    'img/3.enemy/7.dead/Troll_03_1_DIE_001.png',
+    'img/3.enemy/7.dead/Troll_03_1_DIE_002.png',
+    'img/3.enemy/7.dead/Troll_03_1_DIE_003.png',
+    'img/3.enemy/7.dead/Troll_03_1_DIE_004.png',
+    'img/3.enemy/7.dead/Troll_03_1_DIE_005.png',
+    'img/3.enemy/7.dead/Troll_03_1_DIE_006.png',
+    'img/3.enemy/7.dead/Troll_03_1_DIE_007.png',
+    'img/3.enemy/7.dead/Troll_03_1_DIE_008.png',
+    'img/3.enemy/7.dead/Troll_03_1_DIE_009.png',
+  ];
+
+  IMAGES_ATTACKING = [
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_000.png',
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_001.png',
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_002.png',
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_003.png',
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_004.png',
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_005.png',
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_006.png',
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_007.png',
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_008.png',
+    'img/3.enemy/5.attack/Troll_03_1_ATTACK_009.png',
+  ];
+
+  constructor() {
+    super().loadImage("img/3.enemy/1.idle/Troll_03_1_IDLE_000.png");
+    this.loadImages(this.IMAGES_WALKING);
+    this.moveLeft();
+    this.x = 600 + Math.random() * 500;
+    this.speed = 0.15 + Math.random() * 0.25;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.moveLeft();
+    }, 1000 / 60);
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_WALKING);
+    }, 100);
+  }
 }
